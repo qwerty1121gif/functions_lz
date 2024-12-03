a6b2704efbbe51f617665e69a8a7feded9935f4a
qwerty1121gif <Vladimirstalin@tutamail.com>
2024-12-03 16:33:34 +0300

    circle

--- circle.py
+++ circle.py
@@ -2,8 +2,8 @@ import math
 
 r = float(input("Введите радиус круга: "))
 
-area = math.pi * r**2
+circarea = math.pi * r**2
 circlen = 2 * math.pi * r
 
-print(f"Площадь круга с радиусом {r:.2f}: {area:.2f}")
+print(f"Площадь круга с радиусом {r:.2f}: {circarea:.2f}")
 print(f"Длина окружности с радиусом {r:.2f}: {circlen:.2f}")
